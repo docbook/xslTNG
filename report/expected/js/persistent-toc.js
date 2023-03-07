af0386034c27ba061c4edea0293e031640406398
ndw <ndw@nwalsh.com>
2023-03-07 18:25:53 +0000

    Deploying to gh-pages from @ docbook/xslTNG@4406140feebffe57ad9c9754d6b06944ea232442 🚀

--- report/expected/js/persistent-toc.js
+++ report/expected/js/persistent-toc.js
@@ -1,4 +1,4 @@
-/* DocBook xslTNG version 2.0.11-SNAPSHOT
+/* DocBook xslTNG version 2.0.11
  *
  * This is persistent-toc.js providing support for the ToC popup
  *
--- report/js/report.js
+++ report/js/report.js
@@ -1,4 +1,4 @@
-/* DocBook xslTNG version 2.0.11-SNAPSHOT
+/* DocBook xslTNG version 2.0.11
  *
  * This is report.js.
  *
