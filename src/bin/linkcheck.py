4481d6580f0ed07acac76186c552a1343aec5f19
Norman Walsh <ndw@nwalsh.com>
2023-04-07 13:28:38 +0100

    Improved the link checker

--- src/bin/linkcheck.py
+++ src/bin/linkcheck.py
@@ -14,10 +14,10 @@ import lxml.etree
 import click
 from saxonche import PySaxonProcessor
 
-def linkcheck(root, debug):
+def linkcheck(root, server_root, debug):
     if debug:
         print("Checking links in", root)
-    
+
     globpatn = root + '/**/*'
     if root.endswith('/'): # can this happen?
         globpatn = root + '**/*'
@@ -32,10 +32,18 @@ def linkcheck(root, debug):
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
                 xmlns:xs="http://www.w3.org/2001/XMLSchema"
                 xmlns:h="http://www.w3.org/1999/xhtml"
-                exclude-result-prefixes="xs"
+                xmlns:f="http://nwalsh.com/ns/functions"
+                exclude-result-prefixes="#all"
                 version="3.0">
 
 <xsl:output method="json" encoding="utf-8"/>
+
+<xsl:param name="server-root" as="xs:string?" select="()"/>
+
+<xsl:variable name="server-base-uri" as="xs:string?"
+              select="if (empty($server-root))
+                      then ()
+                      else resolve-uri($server-root, base-uri(.)) || '/'"/>
 
 <xsl:mode on-no-match="shallow-skip"/>
 
@@ -48,19 +56,47 @@ def linkcheck(root, debug):
 </xsl:template>
 
 <xsl:template match="*[@href]" as="xs:string">
-  <xsl:sequence select="resolve-uri(@href, base-uri(.))"/>
+  <xsl:sequence select="f:resolve(@href, .)"/>
+  <xsl:if test="(self::h:a or self::a) and empty(node())">
+    <xsl:message select="'Unclickable:', string(@href), 'in', base-uri(.)"/>
+  </xsl:if>
 </xsl:template>
 
 <xsl:template match="*[@src]" as="xs:string">
-  <xsl:sequence select="resolve-uri(@src, base-uri(.))"/>
+  <xsl:sequence select="f:resolve(@src, .) "/>
 </xsl:template>
+
+<xsl:function name="f:resolve" as="xs:string">
+  <xsl:param name="href" as="xs:string"/>
+  <xsl:param name="context" as="element()"/>
+
+  <!-- work around for bug in SaxonC HE 12.1 -->
+  <xsl:variable name="baseuri"
+                select="if (contains(base-uri($context), '/file:'))
+                        then 'file:' || substring-after(base-uri($context), '/file:')
+                        else base-uri($context)"/>
+
+  <xsl:variable name="res" as="xs:string">
+    <xsl:choose>
+      <xsl:when test="exists($server-base-uri) and starts-with($href, '/')">
+        <xsl:sequence select="$server-base-uri || substring($href, 2)"/>
+      </xsl:when>
+      <xsl:otherwise>
+        <xsl:sequence select="resolve-uri($href, $baseuri)"/>
+      </xsl:otherwise>
+    </xsl:choose>
+  </xsl:variable>
+
+  <!--<xsl:message select="$href, '→', $res"/>-->
+  <xsl:sequence select="$res"/>
+</xsl:function>
 
 </xsl:stylesheet>
 """
 
     seen = []
     fail = {}
-    
+
     for htmlfile in files:
         with open(htmlfile, "r", encoding="utf-8") as html:
             doc = html5_parser.parse(html.read())
@@ -69,13 +105,17 @@ def linkcheck(root, debug):
 
         with PySaxonProcessor(license=False) as saxon:
             xslt = saxon.new_xslt30_processor()
+            if server_root:
+                xslt.set_parameter("{}server-root", saxon.make_string_value(root))
             xexec = xslt.compile_stylesheet(stylesheet_text=extract)
 
             builder = saxon.new_document_builder()
             builder.set_base_uri('file:' + htmlfile)
             node = builder.parse_xml(xml_text=text)
 
-            uris = json.loads(xexec.apply_templates_returning_string(xdm_value=node))
+            xexec.set_global_context_item(xdm_item=node)
+            result = xexec.apply_templates_returning_string(xdm_value=node)
+            uris = json.loads(result)
 
             for uri in uris:
                 if uri.startswith('file:'):
@@ -84,8 +124,14 @@ def linkcheck(root, debug):
                     # FIXME: check fragment identifiers...
                     if "#" in fn:
                         fn = fn[0:fn.index("#")]
-                    
+
+                    # FIXME?: check queries
+                    if "?" in fn:
+                        fn = fn[0:fn.index("?")]
+
                     if fn not in seen:
+                        if debug:
+                            print(f"\tCheck {uri}")
                         seen.append(fn)
                         if not os.path.exists(fn):
                             if not htmlfile in fail:
@@ -102,10 +148,11 @@ def linkcheck(root, debug):
 
 @click.command()
 @click.option("--debug", help="Debug", is_flag=True)
+@click.option("--server-root", "-s", help="Server root", is_flag=True)
 @click.argument("root", type=click.Path(exists=True))
-def main(debug, root):
+def main(debug, server_root, root):
     """ The main entry point."""
-    linkcheck(os.path.abspath(root), debug)
+    linkcheck(os.path.abspath(root), server_root, debug)
 
 if __name__ == "__main__":
     main()
