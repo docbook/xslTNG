b9d8f809283369989ea7f2d8ac51f8546c921350
ndw <ndw@nwalsh.com>
2023-06-12 10:44:27 +0000

    Deploying to gh-pages from @ docbook/xslTNG@8e15c8a9f1804b54f2f0355461b1b04f69b212a7 🚀

--- report/expected/js/chunk-nav.js
+++ report/expected/js/chunk-nav.js
@@ -1,4 +1,4 @@
-/* DocBook xslTNG version 2.1.6
+/* DocBook xslTNG version 2.1.6-SNAPSHOT
  *
  * This is chunk-nav.js providing support for keyboard
  * navigation between chunks.
--- report/expected/js/pagetoc.js
+++ report/expected/js/pagetoc.js
@@ -1,4 +1,4 @@
-/* DocBook xslTNG version 2.1.6
+/* DocBook xslTNG version 2.1.6-SNAPSHOT
  *
  * This is pagetoc.js providing support for on-page ToCs.
  *
