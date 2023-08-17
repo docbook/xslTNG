b3e2df6eef68402fbb22f3d64cab81f42e1c72c8
ndw <ndw@nwalsh.com>
2023-08-17 17:42:09 +0000

    Deploying to gh-pages from @ docbook/xslTNG@30762c21d8e944e3aa3b891e5ab2aa3744ad7eac 🚀

--- report/expected/js/xlink.js
+++ report/expected/js/xlink.js
@@ -1,4 +1,4 @@
-/* DocBook xslTNG version 2.1.8-SNAPSHOT
+/* DocBook xslTNG version 2.1.8
  *
  * This is xlink.js providing support for multi-targeted links
  *
--- report/js/report.js
+++ report/js/report.js
@@ -1,4 +1,4 @@
-/* DocBook xslTNG version 2.1.8-SNAPSHOT
+/* DocBook xslTNG version 2.1.8
  *
  * This is report.js.
  *
